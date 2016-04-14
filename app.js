d0727e4fe3bb0fd8e3439d0f6f8adf1ea82054a8
wanhrust <rustam.alashrafov@gmail.com>
2016-04-14 20:02:38 +0200

    Added .env

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 var app      = express(); 
 var ipaddress = "127.0.0.1";
 var http = require('http');
-var port = 8080;
+var port = process.env.PORT || 8080;
 // Configuration 
 app.use(express.static(__dirname + '/public')); 
 app.use(connect.logger('dev')); 
